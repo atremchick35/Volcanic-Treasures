782534d93ff64451219d367fba7c528b43a23bac
artemchick35 <artemryzhkov2004@gmail.com>
2024-05-04 14:44:21 +0500

    Add_Flamethrower_Workpiece

---
+++ Assets/Scripts/FlameThrower.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using Interfaces;
+using Player_Scripts;
+using UnityEngine;
+
+public class FlameThrower : MonoBehaviour, ITrapable
+{
+    private Collider2D _fireCollider2D;
+    private Player _player;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _fireCollider2D = GetComponent<Collider2D>();
+        _player = GameObject.FindWithTag("Player").GetComponent<Player>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void FireOn()
+    {
+        _fireCollider2D.enabled = true;
+    }
+
+    public void FireOff()
+    {
+        _fireCollider2D.enabled = false;
+    }
+    
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+            KillPlayer();
+    }
+
+    public void KillPlayer() => _player.Death();
+}
