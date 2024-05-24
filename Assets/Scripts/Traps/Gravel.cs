29bc5b6084a9e2783bb6cb7237f48fefca67dbc6
korjeek <olegka823@gmail.com>
2024-05-25 00:10:55 +0500

    addition to previous commit

--- Assets/Scripts/Traps/Gravel.cs
+++ Assets/Scripts/Traps/Gravel.cs
@@ -7,23 +7,20 @@ namespace Traps
     public class Gravel : MonoBehaviour, ITrapable
     {
         [FormerlySerializedAs("Delay")] [SerializeField] private float delay;
-        
-        private Collider2D _collider2D;
     
         // Start is called before the first frame update
         void Start()
         {
-            _collider2D = GetComponent<Collider2D>();
         }
 
         public void OnTriggerEnter2D(Collider2D other)
         {
-            if (other.CompareTag("Player"))
-            {
-                Debug.Log("Gravel crumbling");
-                _collider2D.isTrigger = false;
+        }
+
+        public void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.gameObject.CompareTag("Player"))
                 Destroy(gameObject, delay);
-            }
         }
     }
 }
