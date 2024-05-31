0230fc8a5330f6a7978f0072a07ed41e9de5e56c
korjeek <olegka823@gmail.com>
2024-05-31 09:16:33 +0500

    Revert "changes"
    This reverts commit 5e3a631ce73b9404d42cd5219af139593765ee9d.

--- Assets/Scripts/Interfaces/IBuffable.cs
+++ Assets/Scripts/Interfaces/IBuffable.cs
@@ -1,6 +1,7 @@
 using Interfaces;
 using Player_Scripts;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Interfaces
 {
@@ -21,30 +22,33 @@ namespace Buffs
         private const float UsingTime = 10f;
         protected Player Player;
         protected Movement Movement;
+        protected Canvas Canvas;
+
 
         private void Awake()
         {
             var player = GameObject.FindWithTag("Player");
             Player = player.GetComponent<Player>();
             Movement = player.GetComponent<Movement>();
+            Canvas = GameObject.FindWithTag("Canvas").GetComponent<Canvas>();
         }
 
         public abstract void AddBuff();
-
+        
         public abstract void RemoveBuff();
-
+        
+        public abstract Transform GetImage();
+        
         public void GivePlayer()
         {
             AddBuff();
-
+            
             if (Player.Effects.ContainsKey(GetImage()))
                 Player.Effects[GetImage()] = UsingTime;
             else
                 Player.Effects.Add(GetImage(), UsingTime);
             
             Debug.Log("Item Given");
-
-            
             Invoke(nameof(RemoveBuff), UsingTime);
         }
     }
