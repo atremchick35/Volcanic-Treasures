97cdc273e219c700050d28fe72d35450d4764e15
artemchick35 <artemryzhkov2004@gmail.com>
2024-06-02 13:24:00 +0500

    FIX

--- Assets/Scripts/Buffs/Boots.cs
+++ Assets/Scripts/Buffs/Boots.cs
@@ -5,9 +5,21 @@ namespace Buffs
     // Реализация "ботинок"
     public class Boots : LootBuffs
     {
-        protected override void AddBuff() => Movement.SetSpeed(Fields.Buffs.BootsAcceleration);
+        private bool _isClaimed;
+        protected override void AddBuff()
+        {
+            if (!_isClaimed)
+            {
+                Movement.SetSpeed(Fields.Buffs.BootsAcceleration);
+                _isClaimed = true;
+            }
+        }
 
-        protected override void RemoveBuff() => Movement.ResetSpeed(Fields.Buffs.BootsAcceleration);
+        protected override void RemoveBuff()
+        {
+            Movement.ResetSpeed(Fields.Buffs.BootsAcceleration);
+            _isClaimed = false;
+        }
         
         protected override Transform GetImage() => Canvas.transform.GetChild(0).GetChild(0);
     }
